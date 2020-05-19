9c6bc49d0b6bd9aa618d41de047a6886d1381be4
Toby Chow <tobychow98@gmail.com>
2020-05-19 14:36:48 -0500

    Updates

---
+++ precache-manifest.19778ab7dd5f2fb9cbb80f36fffe20b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590153faacf1b602189cf7f7d8b7f80f",
+    "url": "/utd-parking-assist/index.html"
+  },
+  {
+    "revision": "a097229c8562d7cd629a",
+    "url": "/utd-parking-assist/static/css/main.bcd977a8.chunk.css"
+  },
+  {
+    "revision": "b86bb35a6b9fa87dea0b",
+    "url": "/utd-parking-assist/static/js/2.91dfed44.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/utd-parking-assist/static/js/2.91dfed44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a097229c8562d7cd629a",
+    "url": "/utd-parking-assist/static/js/main.d3a2d45f.chunk.js"
+  },
+  {
+    "revision": "09a6c55079a3294c587a",
+    "url": "/utd-parking-assist/static/js/runtime-main.7d30bfb3.js"
+  }
+]);
