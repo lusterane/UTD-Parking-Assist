3ed9b4c299410ef8aa80181158658565b801a267
Toby Chow <tobychow98@gmail.com>
2020-05-19 13:23:18 -0500

    Modifications for heroku hosting

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+require('dotenv').config({ path: './dev.env' });
 
-const parkingStructuresRoute = require("./api/routes/parkingStructures");
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const parkingStructuresRoute = require('./api/routes/parkingStructures');
 
 mongoose.connect(
-	"mongodb+srv://lusterane:" +
+	'mongodb+srv://lusterane:' +
 		process.env.MONGO_ATLAS_PW +
-		"@utd-parking-assist-fo6hm.mongodb.net/utd_parking?retryWrites=true&w=majority",
+		'@utd-parking-assist-fo6hm.mongodb.net/utd_parking?retryWrites=true&w=majority',
 	{
 		useUnifiedTopology: true,
 		useNewUrlParser: true,
@@ -17,32 +19,32 @@ mongoose.connect(
 );
 
 // middleware
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // fix CORS error
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*"); // give access to any origin
+	res.header('Access-Control-Allow-Origin', process.env.HOST_ADDRESS); // give access to any origin
 	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 	);
 
-	if (req.method == "OPTIONS") {
-		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+	if (req.method == 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
 		return res.status(200).json({});
 	}
 	next();
 });
 
-app.use("/parkingStructures", parkingStructuresRoute);
+app.use('/parkingStructures', parkingStructuresRoute);
 
 // error handling
 
 // 404 Not Found Error
 app.use((req, res, next) => {
-	const err = new Error("Not Found");
+	const err = new Error('Not Found');
 	err.status = 404;
 	next(err); // forwards err to next middleware
 });
