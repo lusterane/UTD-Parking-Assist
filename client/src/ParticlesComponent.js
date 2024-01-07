96a29420371eab61231504434a49346cd4adddf7
Toby Chow <tobychow98@gmail.com>
2024-01-07 12:37:47 -0600

    reduce overhead for particles

--- client/src/ParticlesComponent.js
+++ client/src/ParticlesComponent.js
@@ -1,24 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import Particles, { initParticlesEngine } from '@tsparticles/react';
-import { loadSlim } from '@tsparticles/slim';
+import React from 'react';
+import Particles from '@tsparticles/react';
 
 import './App.css';
 import particlesOptions from './particles.json';
 
 function ParticlesComponent() {
-	const [init, setInit] = useState(false);
-
-	useEffect(() => {
-		if (init) {
-			return;
-		}
-
-		initParticlesEngine(async (engine) => {
-			await loadSlim(engine);
-		}).then(() => {
-			setInit(true);
-		});
-	}, []);
 	return <Particles options={particlesOptions} />;
 }
 
