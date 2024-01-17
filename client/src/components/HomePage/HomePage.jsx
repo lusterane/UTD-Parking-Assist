72a67e90c05789fdad1686ba244ea35a68fd53d1
Toby <tobychow98@gmail.com>
2024-01-16 18:16:45 -0600

    fix footer

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ColorOption from './ColorOption/ColorOption';
 import ConfirmModal from './ConfirmModal/ConfirmModal';
-
+import Footer from '../Footer/Footer';
 import './HomePageStyle.css';
 import '../../styles/shared/Colors.css';
 
@@ -24,78 +24,77 @@ function Home(props) {
 
 	return (
 		<React.Fragment>
-			<div id="center-text" className="dark-mode">
-				<div className="content-container">
-					<div className="inner-content-container">
-						<div className="text-content">
-							<h1 className={props.color + ' title-greeting'}>UTD Parking</h1>
-							<div className="sub-title-greeting">
-								<span>Park Smarter, Not Harder</span>
-							</div>
+			<div id="center-text" className=" dark-mode content-container">
+				<div className="inner-content-container">
+					<div className="text-content">
+						<h1 className={props.color + ' title-greeting'}>UTD Parking</h1>
+						<div className="sub-title-greeting">
+							<span>Park Smarter, Not Harder</span>
 						</div>
-						<div className="color-option-container">
-							<ColorOption
-								color="gold"
-								colorBlindMode={props.colorBlindMode}
-								onClick={() => {
-									handleModalShow(props.color);
-								}}
-								handleMouseOver={props.changeColor}
-								handleMouseLeave={() => {
-									if (!showModal) {
-										props.changeColorDefault();
-									}
-								}}
-							/>
-							<ColorOption
-								color="orange"
-								colorBlindMode={props.colorBlindMode}
-								onClick={() => {
-									handleModalShow(props.color);
-								}}
-								handleMouseOver={props.changeColor}
-								handleMouseLeave={() => {
-									if (!showModal) {
-										props.changeColorDefault();
-									}
-								}}
-							/>
-							<ColorOption
-								color="purple"
-								colorBlindMode={props.colorBlindMode}
-								onClick={() => {
-									handleModalShow(props.color);
-								}}
-								handleMouseOver={props.changeColor}
-								handleMouseLeave={() => {
-									if (!showModal) {
-										props.changeColorDefault();
-									}
-								}}
-							/>
-							<ColorOption
-								color="payBySpace"
-								colorBlindMode={props.colorBlindMode}
-								onClick={() => {
-									handleModalShow(props.color);
-								}}
-								handleMouseOver={props.changeColor}
-								handleMouseLeave={() => {
-									if (!showModal) {
-										props.changeColorDefault();
-									}
-								}}
-							/>
-						</div>
-						<ConfirmModal
-							color={props.color}
-							showModal={showModal}
-							onModalShow={handleModalShow}
-							onModalClose={handleModalClose}
-							onModalConfirm={handleModalConfirm}
+					</div>
+					<div className="color-option-container">
+						<ColorOption
+							color="gold"
+							colorBlindMode={props.colorBlindMode}
+							onClick={() => {
+								handleModalShow(props.color);
+							}}
+							handleMouseOver={props.changeColor}
+							handleMouseLeave={() => {
+								if (!showModal) {
+									props.changeColorDefault();
+								}
+							}}
+						/>
+						<ColorOption
+							color="orange"
+							colorBlindMode={props.colorBlindMode}
+							onClick={() => {
+								handleModalShow(props.color);
+							}}
+							handleMouseOver={props.changeColor}
+							handleMouseLeave={() => {
+								if (!showModal) {
+									props.changeColorDefault();
+								}
+							}}
+						/>
+						<ColorOption
+							color="purple"
+							colorBlindMode={props.colorBlindMode}
+							onClick={() => {
+								handleModalShow(props.color);
+							}}
+							handleMouseOver={props.changeColor}
+							handleMouseLeave={() => {
+								if (!showModal) {
+									props.changeColorDefault();
+								}
+							}}
+						/>
+						<ColorOption
+							color="payBySpace"
+							colorBlindMode={props.colorBlindMode}
+							onClick={() => {
+								handleModalShow(props.color);
+							}}
+							handleMouseOver={props.changeColor}
+							handleMouseLeave={() => {
+								if (!showModal) {
+									props.changeColorDefault();
+								}
+							}}
 						/>
 					</div>
+					<ConfirmModal
+						color={props.color}
+						showModal={showModal}
+						onModalShow={handleModalShow}
+						onModalClose={handleModalClose}
+						onModalConfirm={handleModalConfirm}
+					/>
 				</div>
+				{/* <Footer /> */}
 			</div>
 		</React.Fragment>
 	);
