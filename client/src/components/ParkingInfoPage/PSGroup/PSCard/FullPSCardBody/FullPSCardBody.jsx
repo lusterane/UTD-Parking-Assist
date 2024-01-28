22fc822233f0f3ff01835efd98dea02e6b57167a
Toby <tobychow98@gmail.com>
2024-01-28 13:39:34 -0600

    put hippo in

--- client/src/components/ParkingInfoPage/PSGroup/PSCard/FullPSCardBody/FullPSCardBody.jsx
+++ client/src/components/ParkingInfoPage/PSGroup/PSCard/FullPSCardBody/FullPSCardBody.jsx
@@ -7,7 +7,9 @@ class FullPSCardBody extends Component {
 		return (
 			<React.Fragment>
 				<div className="full-text-container">
-					<p className="full-title">FULL</p>
+					<p className="full-title">
+						<i class="fas fa-hippo"></i>
+					</p>
 					<p className="full-subtext">Sorry, this parking garage is unavailable!</p>
 				</div>
 			</React.Fragment>
